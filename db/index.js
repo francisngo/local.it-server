55872398d70093355482effcbfb9c137bea25d65
Scott Schaefer <scottschaef@gmail.com>
2017-09-01 15:21:00 -0400

    Create User schema and test for successful user add

--- db/index.js
+++ db/index.js
@@ -7,5 +7,5 @@ mongoose.connect(url, {
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
-  console.log('Database connected sucessfully.');')
+  console.log('Database connected sucessfully.');
 });
---
+++ db/models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require ('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = Schema({
+  user: String,
+  interestsByCity: [{ city: String, interests: Object, dislikedInterests: Object }],
+  itineraryByCity: [{ name: String, itineraryList: Object }]
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
