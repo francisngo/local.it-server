5df0468ba565ba0f419857c35bf91d8bf32cd90a
Francis Ngo <francis.t.ngo@gmail.com>
2017-09-01 14:13:31 -0700

    Seed businesses data for testing

---
+++ test/test-seed.js
@@ -0,0 +1,72 @@
+module.exports.sampleBusiness1 = {
+  "id": "dinosaur-coffee-los-angeles",
+  "name": "Dinosaur Coffee",
+  "image_url": "https://s3-media2.fl.yelpcdn.com/bphoto/5CHIY8Iyu4S-uPHYiha9xQ/o.jpg",
+  "is_closed": false,
+  "url": "https://www.yelp.com/biz/dinosaur-coffee-los-angeles?adjust_creative=TJCJBQCAAm_aB5D00Y6-UQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=TJCJBQCAAm_aB5D00Y6-UQ",
+  "review_count": 373,
+  "categories": [
+    {
+      "alias": "coffee",
+      "title": "Coffee & Tea"
+    }
+  ],
+  "rating": 4,
+  "coordinates": {
+    "latitude": 34.0952683206945,
+    "longitude": -118.283839609328
+  },
+  "transactions": [],
+  "price": "$$",
+  "location": {
+    "address1": "4334 W Sunset Blvd",
+    "address2": "",
+    "address3": "",
+    "city": "Los Angeles",
+    "zip_code": "90029",
+    "country": "US",
+    "state": "CA",
+    "display_address": ["4334 W Sunset Blvd", "Los Angeles, CA 90029"]
+  },
+  "phone": "",
+  "display_phone": "",
+  "distance": 5097.226532784
+}
+
+module.exports.sampleBusiness2 = {
+  "id": "coffee-commissary-los-angeles-2",
+  "name": "Coffee Commissary",
+  "image_url": "https://s3-media2.fl.yelpcdn.com/bphoto/vK0TFzDnnl4yPtJ33IdYJw/o.jpg",
+  "is_closed": false,
+  "url": "https://www.yelp.com/biz/coffee-commissary-los-angeles-2?adjust_creative=TJCJBQCAAm_aB5D00Y6-UQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=TJCJBQCAAm_aB5D00Y6-UQ",
+  "review_count": 81,
+  "categories": [
+    {
+      "alias": "coffee",
+      "title": "Coffee & Tea"
+    }, {
+      "alias": "bakeries",
+      "title": "Bakeries"
+    }
+  ],
+  "rating": 4,
+  "coordinates": {
+    "latitude": 34.098168357026,
+    "longitude": -118.321811446915
+  },
+  "transactions": [],
+  "price": "$",
+  "location": {
+    "address1": "6087 W Sunset Blvd",
+    "address2": "",
+    "address3": "",
+    "city": "Los Angeles",
+    "zip_code": "90028",
+    "country": "US",
+    "state": "CA",
+    "display_address": ["6087 W Sunset Blvd", "Los Angeles, CA 90028"]
+  },
+  "phone": "+13234673559",
+  "display_phone": "(323) 467-3559",
+  "distance": 4067.5997689039996
+}
