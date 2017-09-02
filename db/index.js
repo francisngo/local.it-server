36ca9dc1cef5757172537f5ec5f6f42f9c5631c3
Scott Schaefer <scottschaef@gmail.com>
2017-09-01 20:04:22 -0400

    Create post handling for new user

--- db/index.js
+++ db/index.js
@@ -9,3 +9,5 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
   console.log('Database connected sucessfully.');
 });
+
+module.exports.db;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,27 @@
+const db = require('../db');
+const User = require('../db/models/User')
+
 module.exports = (app) => {
 
   app.get('/', (req, res) => {
     res.send('Hello, world');
+  });
+
+  // post new user to db
+  app.post('/api/user', (req, res) => {
+    console.log(req.body)
+    const name = req.body.user
+    const newUser = new User({
+      user: name
+    });
+    newUser.save((err, user) => {
+      if (err) {
+        res.json(err);
+      } else {
+        console.log('pass')
+        res.json(user);
+      }
+    });
   });
 
 }
