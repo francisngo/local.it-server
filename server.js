f29c534199bdbcaf280b0681d7b40beba8869bb7
Francis Ngo <francis.t.ngo@gmail.com>
2017-09-02 00:36:19 -0700

    Install morgan for server logging

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const PORT = 3000;
+const morgan = require('morgan');
+const PORT = process.env.PORT || 3000;
 const routes = require('./routes');
 
 //parse json
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
 routes(app);
 
-app.listen(PORT, () => {
+app.listen(PORT, (err) => {
+  if (err) { console.error(err); }
   console.log(`Server listening on port: ${PORT}`);
 });
