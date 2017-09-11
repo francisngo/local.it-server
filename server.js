a7e82b2a367cfb4f6af33263e89c7a0d82c016ed
Nick <nhattwick@gmail.com>
2017-09-11 10:29:03 -0700

    rebase

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const session = require('express-session');
+>>>>>>> rebase
 const { router, passport } = require('./router');
 
 const app = express();
@@ -16,6 +17,7 @@ app.use(session({
   saveUninitialized: true
 }));
 // set Passport middleware
+>>>>>>> rebase
 app.use(passport.initialize());
 app.use(passport.session());
 
