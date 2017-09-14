6b9bbed3c78f6550040ce575297d439fd3b1e04e
Christine Zimmerman <ChristineOrtega@gmail.com>
2017-09-14 16:00:24 -0700

    Add a slight change to router/index.js before commit

--- router/index.js
+++ router/index.js
@@ -92,9 +92,10 @@ router.get('/logout', (req, res, next) => {
   });
 });
 
-// get a user's Saved Interests by City from DB
+// get a user from DB
 router.get('/api/:user', (req, res) => {
   var user = req.params.user;
+  console.log(user);
   User.findOne({ fbID: user }, (err, user) => {
     if (err) {return console.error(err)}
     res.json(user);
